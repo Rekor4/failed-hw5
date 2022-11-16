2f163101a665480e2e3c4eee0b63b49e75415a25
Rekor4 <yegor.miskel1@gmail.com>
2022-11-16 17:27:13 +0300

    enter

--- index.js
+++ index.js
@@ -4,29 +4,37 @@ fetch('https://dummyjson.com/products')
     console.log(data.products);
     const products = data.products;
 
-    const divFirst = document.createElement('div');
-    divFirst.classList.add('container');
-    const divFirstPos = document.getElementsByClassName('wrapper')[0];
-    divFirstPos.appendChild(divFirst);
+    // const divFirstPos = document.getElementsByClassName('wrapper')[0];
 
-    const divRow = document.createElement('div');
-    divRow.classList.add('row', 'row-cols-4');
-    // divRow.classList.add('card-group')
-    divFirst.appendChild(divRow);
+    // const divFirst = document.createElement('div');
+    // divFirst.classList.add('container');
+    // const divFirstPos = document.getElementsByClassName('wrapper')[0];
+    // divFirstPos.appendChild(divFirst);
+
 
     products.forEach((products) => {
+
+      const divColumn = document.createElement('div');
+      divColumn.classList.add('col-sm-12', 'col-md-3', 'column-item');
+      const divColumnPos = document.getElementsByClassName('wrapper')[0]
+      divColumnPos.appendChild(divColumn);
+      
       const divCard = document.createElement('div');
-      divCard.classList.add('card', 'flex-fill', 'col-md-4');
-      divRow.appendChild(divCard);
+      divCard.classList.add('card', 'card-item', 'h-100');
+      divColumn.appendChild(divCard);
+
+      const divImgContainer = document.createElement('div')
+      divImgContainer.classList.add('img-container')
+      divCard.appendChild(divImgContainer)
 
       const img = document.createElement('img');
       img.classList.add('card-img-top', 'img-size'); // create img in fullscr
       img.src = `${products.thumbnail}`;
       img.alt = 'product';
-      divCard.appendChild(img);
+      divImgContainer.appendChild(img);
 
       const divCardBody = document.createElement('div');
-      divCardBody.classList.add('card-body');
+      divCardBody.classList.add('card-body', 'flex-column', 'd-flex'); // d-flex crush
       divCard.appendChild(divCardBody);
 
       const h5 = document.createElement('h5');
@@ -35,7 +43,7 @@ fetch('https://dummyjson.com/products')
       divCardBody.appendChild(h5);
 
       const p = document.createElement('p');
-      p.classList.add('card-text');
+      p.classList.add('card-text', 'flex-grow-1');
       p.textContent = `${products.description}`;
       divCardBody.appendChild(p);
 
@@ -43,10 +51,10 @@ fetch('https://dummyjson.com/products')
       spanFirst.textContent = (`Price: ${products.price}$`);
 
       const spanSecond = document.createElement('span');
-      spanSecond.textContent = (`Rating: ${products.discountPercentage}`);
+      spanSecond.textContent = (`Rating: ${products.rating}`);
 
       const containerForFooter = document.createElement('div');
-      containerForFooter.classList.add('space-around', 'card-footer');
+      containerForFooter.classList.add('space-around', 'card-footer', 'd-flex');
 
       divCardBody.appendChild(containerForFooter);
       containerForFooter.appendChild(spanFirst);
